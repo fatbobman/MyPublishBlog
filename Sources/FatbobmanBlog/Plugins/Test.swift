6b68e06cd782b43cefc041a32febea2115bf382c
Yang Xu <xuyang@me.com>
2021-02-08 07:51:11 +0800

    format

--- Sources/FatbobmanBlog/Plugins/Test.swift
+++ Sources/FatbobmanBlog/Plugins/Test.swift
@@ -1,6 +1,6 @@
 //
 //  File.swift
-//  
+//
 //
 //  Created by Yang Xu on 2021/1/30.
 //
@@ -8,13 +8,13 @@
 import Foundation
 import Publish
 
-extension Plugin{
-    static func test() -> Self{
-        Plugin(name: "test"){ content in
-            for section in content.sections{
-                print(section.content)
-//                section.items.first!.tag
-            }
-        }
+extension Plugin {
+  static func test() -> Self {
+    Plugin(name: "test") { content in
+      for section in content.sections {
+        print(section.content)
+        //                section.items.first!.tag
+      }
     }
+  }
 }
