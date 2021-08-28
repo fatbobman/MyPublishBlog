8d768d32859f4c529617d7b76471db0dd36048f3
Yang Xu <xuyang@me.com>
2021-08-28 08:36:01 +0800

    coreDataInPreview

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-scp -i ~/.ssh/id_rsa -r  ~/Documents/MyPublishBlog/Output root@111.229.200.169:/var/www
+scp -i ~/.ssh/id_rsa -r  ~/Documents/MyPublishBlog/Output root@110.42.133.143:/var/www
 
 
