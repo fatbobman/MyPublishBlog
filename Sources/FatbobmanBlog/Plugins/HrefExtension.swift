0d792e88b62424e8f980b914f4ae6bcb9000bee1
fatbobman <xuyang@me.com>
2023-03-16 08:57:27 +0800

    update

--- Sources/FatbobmanBlog/Plugins/HrefExtension.swift
+++ Sources/FatbobmanBlog/Plugins/HrefExtension.swift
@@ -10,12 +10,12 @@ import Ink
 import Sweep
 
 // 对于非本站页面都在新的tab中打开
-var hrefOpenNewTab = Modifier(target: .links){ html, markdown in
-    guard html.hasPrefix("<a href=\"http") else {return html}
-    guard !html.contains("fatbobman.com") else {return html}
-    var result = html
-    if let index = result.firstIndex(of: ">") {
-        result.insert(contentsOf: " target=\"_blank\"", at: index)
-    }
-    return result
+var hrefOpenNewTab = Modifier(target: .links) { html, _ in
+  guard html.hasPrefix("<a href=\"http") else { return html }
+  guard !html.contains("fatbobman.com") else { return html }
+  var result = html
+  if let index = result.firstIndex(of: ">") {
+    result.insert(contentsOf: " target=\"_blank\"", at: index)
+  }
+  return result
 }
